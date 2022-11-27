bc71dbbd3b08004fef79d322142dcebc39c59c95
Artem Pronin <artemalexp@gmail.com>
2022-11-27 20:24:44 +0300

    changed js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const feedbackForm = document.querySelector('.feedback__form');
 const footerForm = document.querySelector('.footer__form');
-const feedbackButtonForm = feedbackForm.querySelector('.form__button');
-const footerButtonForm = footerForm.querySelector('.form__button');
-const feebackFieldForm = feedbackForm.querySelector('.form__field');
-const footerFieldForm = footerForm.querySelector('.form__field');
+const feedbackButton = feedbackForm.querySelector('.form__button');
+const footerButton = footerForm.querySelector('.form__button');
+const feebackField = feedbackForm.querySelector('.form__field');
+const footerField = footerForm.querySelector('.form__field');
 
-feedbackButtonForm.addEventListener('click', () => {
+feedbackButton.addEventListener('click', () => {
   feedbackForm.addEventListener('submit', (evt) => evt.preventDefault());
 
-  if (feebackFieldForm.checkValidity()) {
-    feedbackButtonForm.innerHTML = `Круто, спасибо за доверие!`;
+  if (feebackField.checkValidity()) {
+    feedbackButton.innerHTML = `Круто, спасибо за доверие!`;
   }
 })
 
-footerButtonForm.addEventListener('click', () => {
+footerButton.addEventListener('click', () => {
   footerForm.addEventListener('submit', (evt) => evt.preventDefault());
 
-  if (footerFieldForm.checkValidity()) {
-    footerButtonForm.innerHTML = `Круто, спасибо за доверие!`;
+  if (footerField.checkValidity()) {
+    footerButton.innerHTML = `Круто, спасибо за доверие!`;
   }
 })
 
